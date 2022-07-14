959e6381033005b62a42b54a96194cb52e3f6b1b
kienphan301 <kienphan30103@gmail.com>
2022-07-14 22:24:01 +0700

    update call python file as module

--- Ep1.py
+++ Ep1.py
@@ -9,10 +9,13 @@ class info:
     def hello(self):
         return "Xin chao " + self.ten
     @classmethod #Sử dụng phương thức classmethod để thay đổi giá trị biến của toàn bộ class
-    def hanh_kiem_Update(cls, hk):
+    def hanh_kiem_Update(cls, hk): #Có thể thay đổi cls = self, nhưng ngta vẫn quen dùng self
         cls.hanh_kiem = hk
 
+"""
+
 code36 = info("Kien", "18", "Nam")
+
 print(code36.ten)
 print("Tuoi:", code36.age)
 print("Gioi tinh:", code36.gender)
@@ -22,3 +25,5 @@ print(code36.hello()) #Cách phổ biến, ngắn gọn
 
 code36.hanh_kiem_Update("Khá") #Cập nhật giá trị biến
 print("Hanh kiem:", code36.hanh_kiem)
+
+"""
---
+++ call_class_as_module.py
@@ -0,0 +1,5 @@
+from Ep1 import info
+
+id = info("Duy Kien", "19", "male")
+
+print(id.age)
